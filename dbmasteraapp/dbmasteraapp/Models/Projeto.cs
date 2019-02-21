f98f83ac25c7e1785112e0fb706c525bcb093f6e
= <=>
2019-02-21 11:20:47 -0300

    .

---
+++ dbmasteraapp/dbmasteraapp/Models/Projeto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dbmasteraapp.Models
+{
+    class Projeto
+    {
+        public int Codigo { get; set; }
+        public int Descricao { get; set; }
+    }
+}
--- dbmasteraapp/dbmasteraapp/Views/ResultadoPesquisa.xaml.cs
+++ dbmasteraapp/dbmasteraapp/Views/ResultadoPesquisa.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using dbmasteraapp.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,34 @@ namespace dbmasteraapp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ResultadoPesquisa : ContentPage
 	{
-		public ResultadoPesquisa ()
+        private ObservableCollection<Chamado> chamados = new ObservableCollection<Chamado>();
+        public ResultadoPesquisa ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            chamados.Add(new Chamado() { Titulo = "teste chamado 1", Codigo = 111111 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 2", Codigo = 222222 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 3", Codigo = 333333 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 4", Codigo = 444444 });
+            listViewChamado.ItemsSource = chamados;
+        }
+
+      
+
+        private void listViewChamado_Refreshing(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewChamado_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            chamados.Add(new Chamado() { Titulo = "teste chamado 1", Codigo = 111111 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 2", Codigo = 222222 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 3", Codigo = 333333 });
+            chamados.Add(new Chamado() { Titulo = "teste chamado 4", Codigo = 444444 });
+            listViewChamado.ItemsSource = chamados;
+
+        }
+    }
 }
